cf8099e2ebf31f2c9917c01d63187029a644eb92
Andrew Byrne <pachakutech@gmail.com>
2025-05-27 10:45:01 -0700

    Initial Commit

---
+++ AuraGalaxy Watch App/AuraGalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraGalaxyApp.swift
+//  AuraGalaxy Watch App
+//
+//  Created by Pachakutech on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct AuraGalaxy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraGalaxy Watch AppTests/AuraGalaxy_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuraGalaxy_Watch_AppTests.swift
+//  AuraGalaxy Watch AppTests
+//
+//  Created by Pachakutech on 5/27/25.
+//
+
+import Testing
+@testable import AuraGalaxy_Watch_App
+
+struct AuraGalaxy_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
