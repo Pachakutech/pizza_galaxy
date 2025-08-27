7054e6de165df0188ea48b4ae2045da637b618bc
Andrew Byrne <pachakutech@gmail.com>
2025-08-26 22:35:40 -0700

    increases sensitivity of the crown rotation

--- AuraGalaxy Watch App/ContentView.swift
+++ AuraGalaxy Watch App/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
             $crownValue,
             from: -100.0,
             through: 100.0,
-            sensitivity: .low,
+            sensitivity: .medium,
             isContinuous: true
         )
         .gesture(
