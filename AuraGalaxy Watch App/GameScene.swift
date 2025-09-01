129776808f4ed0c75cecb3ec3111cee7c822b3b5
Andrew Byrne <pachakutech@gmail.com>
2025-09-01 12:56:21 -0700

    increased margin fudge factor for 44mm watch

--- AuraGalaxy Watch App/GameScene.swift
+++ AuraGalaxy Watch App/GameScene.swift
@@ -52,7 +52,7 @@ class GameScene: SKScene, ObservableObject {
     private let maxCelestialBodies = 34
     private let blackHoleProbability = 0.2
     private var collisionSoundAction: SKAction? // Preloaded sound action
-    private var yMaxOffset: CGFloat { backgroundHeight - 2.1 * size.height }
+    private var yMaxOffset: CGFloat { backgroundHeight - 2.2 * size.height } // the subtracted factor is a fudge factor
     private var centerX: CGFloat { size.width / 2 }
     private var centerY: CGFloat { size.height / 2 }
 
